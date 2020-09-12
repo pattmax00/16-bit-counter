73e80d7c6634fc25453cbd02e80e5aa190349be3
Maximilian <16063560+pattmax00@users.noreply.github.com>
2020-09-12 15:06:39 -0500

    Fix formatting
    mix of 2 spaces and indents changed to 2 spaces.

--- main.c
+++ main.c
@@ -8,16 +8,16 @@ int main(void) {
 
   // Explaination and user input
   printf("Enter positive int between 0 - 65535: ");
-  scanf("%hu", &number);
+  scanf(" %hu", &number);
 
   // Creates and assigns original number for output after calculations
-	short unsigned int originalNumber = number;
+  short unsigned int originalNumber = number;
 
   // Solves bit calculation 16 times (short unsigned int)
-	for (int i = 0; i <= 16; i++){
-	  oneCounter += (number % 2); // Increments counter if bit is one
+  for (int i = 0; i <= 16; i++){
+    oneCounter += (number % 2); // Increments counter if bit is one
     number = number / 2; // Moves on to next bit
-	}
+  }
 
   // Reverses 1 count to 0 count
   int zeroCounter = 16 - oneCounter;
@@ -25,8 +25,8 @@ int main(void) {
   // Display 0 count
   printf("0s in 16-bit binary representation of %hu: %d\n", originalNumber, zeroCounter);
   
-	// Display 1 count
-	printf("1s in 16-bit binary representation of %hu: %d\n", originalNumber, oneCounter);
+  // Display 1 count
+  printf("1s in 16-bit binary representation of %hu: %d\n", originalNumber, oneCounter);
 
   return 0;
 }
